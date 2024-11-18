9c236fdc45540f13df06b2453d7b37d389c4f4fc
Akedil <akedilnrserik@gmail.com>
2024-11-18 17:27:04 +0500

    Add lab2 solution

---
+++ a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int k;
+    cin >> k;
+
+    int min_dist = abs(a[0]-k);
+    int min_index = 0;
+
+    for(int i = 1; i < n; i ++){
+        if(abs(a[i]-k) < min_dist){
+            min_dist = abs(a[i] - k);
+            min_index = i;
+        }
+    }
+
+    cout << min_index;
+
+    return 0;
+}
